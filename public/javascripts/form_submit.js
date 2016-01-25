f80721d0993b3836fb698cce28b7ea4e95fa4a15
Eduardo Rivara <edurivara@gmail.com>
2016-01-25 17:00:18 -0300

    added javascript

---
+++ public/javascripts/form_submit.js
@@ -0,0 +1,16 @@
+function form_submit(url, params) {
+    var form = document.createElement("form");
+    form.setAttribute("method", "POST");
+    form.setAttribute("action", url);
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+            form.appendChild(hiddenField);
+         }
+    }
+    document.body.appendChild(form);
+    form.submit();
+}
